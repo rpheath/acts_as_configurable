b2e596fdbed2fdea0b49a1f68f8aac75bd65dc94
rpheath <rpheath@gmail.com>
2008-10-27 19:33:13 -0400

    added a few specs to ensure proper behavior

--- spec/acts_as_configurable_spec.rb
+++ spec/acts_as_configurable_spec.rb
@@ -1,5 +1,52 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
 describe "ActsAsConfigurable" do
-  it "should execute" do
+  it "should return empty Hash configuration" do
+    Blank.configuration.is_a?(Hash).should be_true
+    Blank.configuration.should == {}
+  end
+  
+  # proves that configuration can work for
+  # multiple classes using the standard settings
+  describe "standard configuration" do
+    it "should respond to 'configuration'" do
+      Settings1.configuration.should_not be_nil
+      Settings2.configuration.should_not be_nil
+    end
+  
+    it "should return a Hash" do
+      Settings1.configuration.is_a?(Hash).should be_true
+      Settings2.configuration.is_a?(Hash).should be_true
+    end
     
+    it "should return the hash of values for passwords" do
+      Settings1.configuration.users.passwords.is_a?(Hash).should be_true
+      Settings2.configuration.users.passwords.is_a?(Hash).should be_true
+    end
+    
+    it "should return two different ranges for each class" do
+      Settings1.configuration.users.subdomains.should == 10
+      Settings2.configuration.users.subdomains.should == 15
+    end
+    
+    it "should return two different values for password attempts" do
+      Settings1.configuration.users.passwords.attempts.should == 3
+      Settings2.configuration.users.passwords.attempts.should == 5
+    end
+  end
+  
+  # proves that the configuration can handle a custom
+  # 'configuration' method, as well as that the values
+  # stored are exclusive to the class they represent
+  describe "custom configuration" do
+    it "should return a hash based on the custom configurator method" do
+      Settings3.configurator.is_a?(Hash).should be_true
+      Settings4.configurator.is_a?(Hash).should be_true
+    end
+    
+    it "should return the correct values for each custom class" do
+      Settings3.configurator.cache_location.should == '/some/path'
+      Settings4.configurator.cache_location.should == '/other/path'
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,46 @@
+require 'rubygems'
+require 'active_support'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'acts_as_configurable')
+
+class MockActiveRecord
+  include RPH::ActsAsConfigurable
+end
+
+class Settings1 < MockActiveRecord
+  acts_as_configurable
+  
+  configuration do |config|
+    config.users.subdomains = 10
+    config.users.passwords.attempts = 3
+  end
+end
+
+class Settings2 < MockActiveRecord
+  acts_as_configurable
+  
+  configuration do |config|
+    config.users.subdomains = 15
+    config.users.passwords.attempts = 5
+  end
+end
+
+class Settings3 < MockActiveRecord
+  acts_as_configurable :with => :configurator
+  
+  configurator do |config|
+    config.cache_location = '/some/path'
+  end
+end
+
+class Settings4 < MockActiveRecord
+  acts_as_configurable :with => :configurator
+  
+  configurator do |config|
+    config.cache_location = '/other/path'
+  end
+end
+
+
+class Blank < MockActiveRecord
+  acts_as_configurable
+end
